83d072a709c57ea6f8fe21b07bbf2aad1ae4ca47
Le Tan Hai <tanhai@MacBook-Pro.local>
2022-11-06 17:02:30 +0700

    add code excercise 1

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -1,7 +1,26 @@
 # TODO: Designate a cloud provider, region, and credentials
-
+provider "aws" {
+  access_key = var.access_key_var
+  secret_key = var.secret_key_var
+  region = var.region_var
+}
 
 # TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
-
+provider "aws_instance" "Udacity_T2" {
+    count = "4"
+    ami = "ami-09d3b3274b6c5d4aa"
+    instance_type = "t2.micro"
+    tag = {
+        Name = "Udacity T2"
+    }
+}
 
 # TODO: provision 2 m4.large EC2 instances named Udacity M4
+provider "aws_instance" "Udacity_M4" {
+  count = "2"
+  ami = "m4.large"
+  instance_type = "ami-09d3b3274b6c5d4aa"
+  tag = {
+    Name = "Udacity M4"
+  }
+}
